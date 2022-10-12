0b54cdd4f7d1ebf4b9d9a7f32205ab13c070e145
jacqmeek <jlmeek@ucsc.edu>
2022-10-11 22:09:08 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,15 @@
+// Pseudocode for creating a form.
+// For users to create an account on a website, they can fill this form.
+// @link   N/A
+// @file   This file defines the MyClass class.
+// @author Tristan Van Dixon, Jacquelyn Meek, Alex Jones.
+// @since  10.11.22
+
+// Print Log-in Page.
+    // Allow user input; email and password information.
+        // Determine if entered information correlates to security data.
+            // Accept entry if data correlates.
+            // Deny entry if data does not correlate.
+        // Allow user input; name, occupation, and biographical information.
+        //Prompt user for account setup confirmation.
+        //Print account creation success screen.
