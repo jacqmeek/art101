7190e400282bd0cec8bb6ff95537eaf46e727092
jacqmeek <jlmeek@ucsc.edu>
2022-10-17 18:29:34 -0700

    push

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Tristan Van Dixon, Jacquelyn Meek, & Alex Jones
+ * Created:   18.10.2022
+ * License: Public Domain
+ */
+
+// Variable Definition
+var make = "Chevrolet";
+var model = "Suburban";
+var color = "White";
+var year = 2003;
+
+// Age Calculation
+var age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
