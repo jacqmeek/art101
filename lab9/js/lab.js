561f56f1ed77bec9689898c2a6302b4842a74b5b
jacqmeek <jlmeek@ucsc.edu>
2022-10-31 17:59:46 -0700

    task3lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Alex Jones, Jacquelyn Meek, Tristan Van Dixon
+ * Created:   30.10.2022
+ * License: Public Domain
+ */
+
+  var outputEl = document.getElementById("output");
+
+  var new1El = document.createElement("p");
+  new1El.innerHTML = "The First of November";
+
+  var new2El = document.createElement("p");
+  new2El.innerHTML = "November 1st, that is today.";
+
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
